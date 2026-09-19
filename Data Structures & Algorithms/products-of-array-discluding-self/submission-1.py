cf4df2b4bec93d29e3f9d552982e86f00216a8ab
Ismael Romero <254156717+ismaelromeroo@users.noreply.github.com>
2026-09-19 18:00:49 -0400

    Update: products-of-array-discluding-self - submission-1

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -1,15 +1,12 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = [1] * len(nums)
-        prefix = 1
-        postfix = 1
+        out = [1]*(len(nums))
+        pre=1
         for i in range(len(nums)):
-           output[i] = prefix
-           prefix *= nums[i]
-        for j in range(len(nums)-1,-1,-1):
-            output[j] = output[j] * postfix
-            postfix *= nums[j]
-        return output
-
-
-
+            out[i] = pre
+            pre *= nums[i]
+        post = 1
+        for i in range(len(nums)-1,-1,-1):
+            out[i] *= post
+            post *= nums[i]
+        return out
